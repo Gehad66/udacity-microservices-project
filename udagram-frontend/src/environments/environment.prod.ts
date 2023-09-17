05b3d8d02d2ceedb56d4f28dd34b68b2fa4376b8
Gehad66 <26491862+Gehad66@users.noreply.github.com>
2023-09-17 11:32:47 +0300

    hosting hashed data
    AWS service is already down & deleted

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://xxx-xx.us-west-xx.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
